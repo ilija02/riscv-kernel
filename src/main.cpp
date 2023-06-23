9974fb5799c0f66e85eae0045d1857fa97a71b1a
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2023-06-23 09:24:16 -0700

    added vectored interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@
 
 extern "C" void trapHandler();
 
-void main(){
+void main() {
     /*char* mem = (char*)MemoryAllocator::get().mem_alloc(1024);
     for(int i = 0; i< 1024; i++) mem[i] = 'a';
     MemoryAllocator::get().mem_free(mem);*/
-    __asm__ volatile ("csrw stvec, %[vector]" : : [vector]"r"(&trapHandler));
-    Riscv::ms_sstatus(0x02);
-    while(1){
-
-    }
+    uint64 volatile handlerAddress = (uint64) &trapHandler | 0x01; //set the base address for interrupts to trap handler and
+    //set mode to 1 (this enables vectored interrupts)
+    __asm__ volatile ("csrw stvec, %[vector]" : : [vector]"r"(handlerAddress));
+    //Riscv::ms_sstatus(0x02);
+    __putc('A');
+    __asm__ volatile ("ecall");
 }
