839285a3f0625bac1154be3bca53db9136b7b599
Ilija <ilija02@protonmail.com>
2023-06-24 06:54:33 -0700

    add printing funcitonalities

---
+++ h/printing.hpp
@@ -0,0 +1,17 @@
+#ifndef PRINTING_HPP
+#define PRINTING_HPP
+
+#include "../h/syscall_c.h"
+#include "../lib/console.h"
+typedef unsigned long uint64;
+
+void printString(char const *string);
+
+char* getString(char *buf, int max);
+
+int stringToInt(const char *s);
+
+void printInt(int xx, int base=10, int sgn=0);
+
+#endif // PRINTING_HPP
+
