e6eb7d5f59cdc9ceaf3cc206279af8cd4cfc834f
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2023-06-24 07:47:10 -0700

    added new and delete operators and appropriate tests

--- src/UnitTest.cpp
+++ src/UnitTest.cpp
@@ -35,3 +35,42 @@ bool UnitTest::test_memory_allocator() {
     printString("\n");
     return old_size==new_size;
 }
+
+bool UnitTest::test_new_delete() {
+
+    MemoryAllocator& instance = MemoryAllocator::get();
+    BlockHeader* free_head = (BlockHeader*)instance.get_free_head();
+    printString("Free head size after initialization: ");
+    uint64  old_size = free_head->size_in_bytes;
+    printInt(old_size);
+    printString("\n");
+
+    TestStruct *t = new TestStruct();
+    t->x = 5;
+    t->y = 10;
+
+    uint64 *arr = new uint64 [1000];
+    for (int i = 0; i <1000; i++){
+        arr[i] = i;
+    }
+    free_head = (BlockHeader*)instance.get_free_head();
+    printString("Free head size after allocating a struct and an array: ");
+    uint64  allocated_size = free_head->size_in_bytes;
+    printInt(allocated_size);
+    printString("\n");
+
+    printString("Number of bytes allocated: ");
+    printInt(old_size-allocated_size);
+    printString("\n");
+    delete t;
+    delete[] arr;
+
+    free_head = (BlockHeader*)instance.get_free_head();
+    printString("Free head size after freeing memory: ");
+    uint64  new_size = free_head->size_in_bytes;
+    printInt(new_size);
+    printString("\n");
+    printString("Test of new and delete completed.\n");
+
+    return old_size==new_size;
+}
