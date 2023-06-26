ebb9c527e6738043441fa004fad3d22f751e7cf0
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2023-06-26 08:07:10 -0700

    added functional thread_create and refactored the logic for performing syscalls

--- src/interruptHandler.cpp
+++ src/interruptHandler.cpp
@@ -1,39 +1,34 @@
 #include "../h/interruptHandler.hpp"
 
 // takes "implicit" arguments as they are in registers a0 to a7
-extern "C" void handleSupervisorTrap(uint64 syscall_id,  void* a1, void* a2, void* a3, void* a4) {
+extern "C" void handleSupervisorTrap(uint64 syscall_id, void *a1, void *a2, void *a3, void *a4) {
   uint64 volatile processSyscall = 0;
   const uint64 volatile scause = RiscV::r_scause();
   if (scause == InterruptCause::IRQ_ILLEGAL_INSTRUCTION) {
-
+    printString("Exception: Illegal instruction\n");
   } else if (scause == IRQ_ILLEGAL_READ_ADDRESS) {
     printString("Exception: Illegal read address\n");
   } else if (scause == IRQ_ILLEGAL_WRITE_ADDRESS) {
     printString("Exception: Illegal write address\n");
-  } else if (scause == IRQ_ILLEGAL_INSTRUCTION) {
-    printString("Exception: Illegal instruction\n");
   } else if (scause == IRQ_SYSCALL_USER_MODE || scause == IRQ_SYSCALL_KERNEL_MODE) {
     processSyscall = 1;
   } else {
-    printString("Unknown exception cuase. \n");
+    printString("Unknown exception cause. \n");
   }
 
-  if (!processSyscall) {
-    RiscV::mc_sip(RiscV::SIP_SSIE); // clear SSIP bit
-    return;
-  }
+  if (!processSyscall) return;
+  // Note: The return value of the corresponding kernel functions is implicitly stored in a0, and then collected from a0 by corresponding C api call.
+  if (syscall_id == SyscallID::MEM_ALLOC)
+    MemoryAllocator::get().mem_alloc((uint64) a1); //a1 - size param
 
-  if (syscall_id == SyscallID::MEM_ALLOC) {
-    void *volatile allocated_chunk = MemoryAllocator::get().mem_alloc((uint64)a1); //a1 - size param
-    RiscV::w_a0(reinterpret_cast<uint64>(allocated_chunk)); // write the return value into a0
-    //return;
-  } else if (syscall_id == SyscallID::MEM_FREE) {
-    uint64 volatile return_code = MemoryAllocator::get().mem_free(a1); //a1 - chunk
-    RiscV::w_a0(return_code);
-  } else if (syscall_id == SyscallID::THREAD_CREATE) {
+  else if (syscall_id == SyscallID::MEM_FREE)
+    MemoryAllocator::get().mem_free(a1); //a1 - chunk
 
-  }
-  RiscV::mc_sip(RiscV::SIP_SSIE); // clear SSIP bit
+  else if (syscall_id == SyscallID::THREAD_CREATE)
+    // a1 - handle, a2 - start_routine, a3 - argument, a4 - allocated stack
+    TCB::create_thread((TCB **) a1, (TCB::Task) a2, a3, (uint64 *) a4);
+
+  // There's no need to clear the SSIP bit as in other handlers because interrupt bit is not set.
 }
 
 uint64 counter = 0;
--- src/syscall_c.cpp
+++ src/syscall_c.cpp
@@ -1,35 +1,36 @@
 
 #include "../h/syscall_c.h"
-void *mem_alloc(size_t size) {
-  void *allocated_chunk = nullptr;
-  RiscV::w_a1(size); //need to write into a1 first because otherwise the value of size will be lost (that's a0 because its the first arg of mem_alloc)
-  RiscV::w_a0(SyscallID::MEM_ALLOC);
+
+/**
+ * Wrapper function for performing a syscall, takes up to 4 generic arguments and a syscall identifier.
+ * and performs an environment call instruction, passing the arguments through registers a1..a4
+ * @param syscall_id
+ * @param a1 first argument for the syscall
+ * @param a2 second argument for the syscall
+ * @param a3 third argument for the syscall
+ * @param a4 fourth argument for the syscall
+ * @return return value of the performed syscall, stored in the a0 register.
+ */
+uint64 perform_syscall(uint64 syscall_id, void* a1 = nullptr, void* a2= nullptr, void* a3= nullptr, void* a4= nullptr){
+  //arguments are passed to the handleSupervisorTrap through registers
+  uint64 return_value;
   RiscV::ecall();
-  allocated_chunk = reinterpret_cast<void *> (RiscV::r_a0());
-  return allocated_chunk;
+  return_value = RiscV::r_a0();
+  return return_value;
+}
+void *mem_alloc(size_t size) {
+  return (void*)perform_syscall(SyscallID::MEM_ALLOC, (void*)size);
 }
 
 int mem_free(void *chunk) {
-  int volatile return_code;
-  RiscV::w_a1(reinterpret_cast<uint64>(chunk));
-  RiscV::w_a0(SyscallID::MEM_FREE);
-  RiscV::ecall();
-  return_code = RiscV::r_a0();
-  return return_code;
+  return (int)perform_syscall(SyscallID::MEM_FREE, chunk);
 }
+
 int thread_create(thread_t *handle, void (*start_routine)(void *), void *arg) {
   uint64 * allocated_stack = nullptr;
   if (start_routine != nullptr) {
     allocated_stack = (uint64 *)mem_alloc(sizeof(uint64)*DEFAULT_STACK_SIZE);
     if (allocated_stack == nullptr) return -1; // stack allocation failed
   }
-  RiscV::w_a4(reinterpret_cast<uint64>(allocated_stack));
-  RiscV::w_a3(reinterpret_cast<uint64>(arg));
-  RiscV::w_a2(reinterpret_cast<uint64>(start_routine));
-  RiscV::w_a1(reinterpret_cast<uint64>(handle));
-  RiscV::w_a0(SyscallID::THREAD_CREATE);
-  RiscV::ecall();
-  uint64 volatile ret_val = RiscV::r_a0();
-  if (ret_val != 0) return -2; // thread creation failed
-  return 0; // thread creation was successful
+  return (int) perform_syscall(SyscallID::THREAD_CREATE , (void*) handle, (void*)start_routine, (void*)arg, (void*) allocated_stack);
 }
