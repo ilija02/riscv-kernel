c176995d3db886393d6a047e2b8fa9b0ad7495a2
Ilija Radosavljevic <45182003+ilija02@users.noreply.github.com>
2023-06-27 10:20:01 -0700

    added a mechanism to halt a cpu when an exception occurs

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
       handlerAddress = (uint64) &trapHandler | 0x01; //set the base address for interrupts to trap handler and
   //set mode to 1 (this enables vectored interrupts)
   RiscV::w_stvec(handlerAddress);
-  _thread::set_user_mode();
+  //_thread::set_user_mode();
   //-------------------------------------
   MemoryAllocator &instance = MemoryAllocator::get();
   BlockHeader *free_head = (BlockHeader *) instance.get_free_head();
@@ -26,7 +26,7 @@ int main() {
 
   UnitTest& TestRunner = UnitTest::get();
 
-  //TestRunner.test_synchronous_context_switching();
+   TestRunner.test_synchronous_context_switching();
   //TestRunner.test_new_delete();
   //TestRunner.test_dequeue();
   TestRunner.test_thread_create();
